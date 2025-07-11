f4d53bb102dd75d91563db89bc068aa60528ed8e
Giacomo Mascher <giacomo.mascher@glassform.ai>
2025-07-11 23:09:23 +0200

    controller object implemented

--- chapter-8/static/battleship.js
+++ chapter-8/static/battleship.js
@@ -1,3 +1,5 @@
+// http://127.0.0.1:5500/head-first-js/chapter-8/battleships.html
+
 let view = {
     displayMessage(msg) {
         let messageArea = document.getElementById("messageArea");
@@ -15,30 +17,30 @@ let model = {
     shipsSunk: 0,
     shipLength: 3,
     ships: [
-        {locations: ["A1", "A2", "A3"],hits: ["", "", ""]},
-        {locations: ["C1", "C2", "C3"],hits: ["", "", ""]},
-        {locations: ["E1", "E2", "E3"],hits: ["", "", ""]},
+        {locations: ["01", "02", "03"],hits: ["", "", ""]},
+        {locations: ["11", "12", "13"],hits: ["", "", ""]},
+        {locations: ["31", "32", "33"],hits: ["", "", ""]},
     ],
 
     fire(guess) {
-        let result = "miss";
+        let result ="miss";
         for (let i = 0; i < this.numShips; i++) {
             let ship = this.ships[i];
             let index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 result = "hit";
                 ship.hits[index] = "hit";
+                view.displayMessage(result.toUpperCase() + "!");
+                view.displayGuess(guess, result);
                 if (this.isSunk(ship)) {
                     this.shipsSunk++;
                     view.displayMessage("You sank a battleship!");
                 }
+                return true
             }
         }
-        view.displayGuess(guess, result);
         view.displayMessage(result.toUpperCase() + "!");
-        if (result === "hit") {
-            return true;
-        }
+        view.displayGuess(guess, result);
         return false;
     },
     isSunk(ships) {
@@ -46,7 +48,38 @@ let model = {
     },
 };
 
-model.fire(53);
-model.fire(0);
-model.fire(6);
-model.fire(22);
+let controller = {
+    guesses: 0,
+    processGuess(guess) {
+        let elaborated_guess = parseGuess(guess)
+        if (elaborated_guess!=null) {
+            result = model.fire(elaborated_guess);
+            if (result == true && model.shipsSunk == model.numShips) {
+                view.displayMessage("You won!!")
+            }
+        }
+    }
+}
+
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    if (guess==null || guess.length!=2) {
+        view.displayMessage("Please enter a valid guess!")
+    } else {
+        let firstChar = guess.charAt(0)
+        let row = alphabet.indexOf(firstChar)
+        let column = guess.charAt(1)
+        if (isNaN(column)) {
+            view.displayMessage("That's not on the board")
+        } else if (column < 0 || column >= model.boardSize) {
+            view.displayMessage("That's not on the board")
+        } else if (row < 0 || row >= model.boardSize) {
+            view.displayMessage("Not on the board")
+        } else {
+            return row + column
+        }
+    }
+    return null
+}
+
+controller.processGuess("A0")
