ac98534373f142d80cb012a514fb91c35bf68683
Giacomo Mascher <giacomo.mascher@glassform.ai>
2025-07-13 18:09:32 +0200

    images onmouseover

--- chapter-9/images-game/static/images.js
+++ chapter-9/images-game/static/images.js
@@ -3,11 +3,17 @@ window.onload = init
 function init() {
     let blurred_images = document.getElementsByTagName("img")
     for (image of blurred_images) {
-        image.onclick = unblurrImage
+        image.onmouseover = unblurrImage
+        image.onmouseout = reblurImage
     }
 }
 
 function unblurrImage(event) {
     image = event.target
     image.src = `static/images/${image.id}.jpg`
+}
+
+function reblurImage(event) {
+    image = event.target
+    image.src = `static/images/${image.id}blur.jpg`
 }
